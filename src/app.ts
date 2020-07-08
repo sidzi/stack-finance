3e3846ca2052ebcf1bd91c338aaef799b33917a0
Sid <coimunity@protonmail.com>
2020-07-09 01:54:30 +0530

    add index.html serving

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,15 @@
 import WebSocket from "ws";
 import config from "./config";
 import "reflect-metadata";
+import express from "express";
 
 async function startServer() {
-  const wss = new WebSocket.Server(
-    {
-      host: "0.0.0.0",
-      port: config.port,
-    },
-    () => {
-      console.log(`Listening on ${config.port}`);
-    }
-  );
+  const app = express();
+  const server = app
+    .use((req, res) => res.sendFile("index.html", { root: __dirname }))
+    .listen(config.port, () => console.log(`Listening on ${config.port}`));
+
+  const wss = new WebSocket.Server({ server });
   await require("./loaders").default(wss);
 }
 
