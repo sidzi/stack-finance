11736e466be8bc34c05e933f04e5388253697290
Sid <coimunity@protonmail.com>
2020-07-09 01:39:01 +0530

    null check

--- src/loaders/requestHandler.ts
+++ src/loaders/requestHandler.ts
@@ -4,7 +4,7 @@ import { ClientService } from "../services/ClientService";
 import { RequestOptions } from "https";
 
 export default function (ws: WebSocket, req: RequestOptions) {
-  const sessionID = req.headers.cookie.toString();
+  const sessionID = req.headers?.cookie?.toString();
   const clientService = Container.get(ClientService);
   clientService.addClient(ws, sessionID);
 }
